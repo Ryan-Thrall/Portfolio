192f83cc11a61a8f98cf1ddb8a6e6494aefea3f9
Ryan-Thrall <ryanthrall04@gmail.com>
2023-09-21 01:27:34 -0600

    Added Home Page Text

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="grid grid-cols-2">
-      <div className="left text-blue-600 flex justify-center">Hello</div>
-      <div className="right flex justify-center">
-        <div className="imageWrapper">
-          <img src="../ryan_image.jpg" alt="Ryan" className="ryanImage" />
-        </div>
+    <div className="grid grid-cols-2 h-screen items-center">
+      <div className="grid grid-rows-3 h-fit items-center justify-center left text-white">
+        <p className="my-0 font-semibold ms-24 montserrat text-blue text-3xl">Hello World!</p>
+        <p className="my-2 font-semibold ms-24 montserrat text-blue text-4xl">I'm Ryan Thrall</p>
+        <p className="my-4 font-semibold ms-24 montserrat text-blue text-5xl">A Full-Stack Web Developer</p>
+
+
+      </div>
+      <div className="right flex justify-center h-screen">
+        <div className="ryanImage flex self-center"></div>
       </div>
     </div>
   )
