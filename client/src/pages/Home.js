33ed6e5c07e5a05b781378f2b016a2f2c18d3363
Ryan-Thrall <ryanthrall04@gmail.com>
2023-09-24 16:41:09 -0600

    Hover effects on icons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import SvgIcon from '@mui/material/SvgIcon';
 import { GitHub, LinkedIn, Email, Phone } from '@mui/icons-material';
 
 function Home() {
   return (
-    <div className="grid grid-cols-2 h-screen items-center">
-      <div className="grid grid-rows-3 h-fit items-center justify-center left text-white">
-        <p className="my-0 font-semibold ms-24 montserrat text-blue text-3xl">Hello World!</p>
-        <p className="my-2 font-semibold ms-24 montserrat text-blue text-4xl">I'm Ryan Thrall</p>
-        <p className="my-4 font-semibold ms-24 montserrat text-blue text-5xl">A Full-Stack Web Developer</p>
+    <div className="grid grid-cols-12 h-screen items-center">
+      <div className="left flex flex-col h-full justify-center col-span-6">
+        <p className="my-0 font-semibold ms-24 montserrat text-blue text-[40pt]">Hello World!</p>
+        <p className="my-2 font-semibold ms-24 montserrat text-blue text-[48pt]">I'm <span className="text-white">Ryan Thrall</span></p>
+        <p className="my-4 font-semibold ms-24 montserrat text-blue text-[64pt]">A <span className="text-white">Full-Stack Web Developer</span></p>
 
 
       </div>
-      <div className="right h-screen flex flex-col justify-center items-center">
+      <div className="right h-screen flex flex-col justify-center items-center col-span-6">
         <div className="ryanImage"></div>
 
-        <div className="flex justify-between w-8/12">
+        <div className="flex justify-between w-8/12 pt-8">
           <GitHub className="icon" sx={{ fontSize: "5rem" }} />
           <LinkedIn className="icon" sx={{ fontSize: "5rem" }} />
           <Email className="icon" sx={{ fontSize: "5rem" }} />
